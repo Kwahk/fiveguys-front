7f4551cadff2dacfbc221c9c9424a58fd77d9986
OziinG <dlajiin@gmail.com>
2024-08-25 19:35:34 +0900

    TopBar Example

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
-})
+
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      src: "/src",
+    },
+  },
+});
