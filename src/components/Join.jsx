366c28a651acc6a6bb7636192de436941dfb5993
OziinG <dlajiin@gmail.com>
2024-08-26 14:33:15 +0900

    join page

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,32 +1,42 @@
+import { Link } from "react-router-dom";
+
 const Join = () => {
   return (
-    <div className="join-container">
-      <form className="join-form">
-        <h2>Sign up</h2>
-        <div className="join-item">
-          <label htmlFor="username">USERNAME</label>
-          <input type="text" id="username" placeholder="atww9dnjf" />
+    <div className="page-container">
+      <div className="background-gradient"></div>
+      <div className="certify-container">
+        <form className="certify-form">
+          <h2 className="form-title">Sign Up</h2>
+          <div className="certify-item">
+            <label htmlFor="username">USERNAME</label>
+            <input type="text" id="username" placeholder="johndoe@example.com" />
+          </div>
+          <div className="certify-item">
+            <label htmlFor="birth">BIRTH</label>
+            <input type="date" id="birth" />
+          </div>
+          <div className="certify-item">
+            <label htmlFor="email">EMAIL ADDRESS</label>
+            <input type="email" id="email" placeholder="rndnjf@example.com" />
+          </div>
+          <div className="certify-item">
+            <label htmlFor="password">PASSWORD</label>
+            <input type="password" id="password" placeholder="********" />
+            <div className="password-icon">{/* Add eye icon here */}</div>
+          </div>
+          <div className="certify-item">
+            <label htmlFor="confirm-password">PASSWORD CONFIRM</label>
+            <input type="password" id="confirm-password" placeholder="********" />
+            <div className="password-icon">{/* Add eye icon here */}</div>
+          </div>
+          <button type="submit" className="signup-button">
+            PROCEED
+          </button>
+        </form>
+        <div className="signup-footer">
+          Already a Member? <Link to="/login">LOG IN NOW</Link>{" "}
         </div>
-        <div className="join-item">
-          <label htmlFor="birth">BIRTH</label>
-          <input type="text" id="birth" placeholder="2001.01.12" />
-        </div>
-        <div className="join-item">
-          <label htmlFor="email">EMAIL ADDRESS</label>
-          <input type="email" id="email" placeholder="rndnjf@example.com" />
-        </div>
-        <div className="join-item">
-          <label htmlFor="password">PASSWORD</label>
-          <input type="password" id="password" placeholder="********" />
-        </div>
-        <div className="join-item">
-          <label htmlFor="confirm-password">PASSWORD CONFIRM</label>
-          <input type="password" id="confirm-password" placeholder="********" />
-        </div>
-        <button type="submit" className="signup-button">
-          SIGN UP
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
