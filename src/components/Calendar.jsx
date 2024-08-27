91fc5fd4e052f2037073e468f09e113bce566445
OziinG <dlajiin@gmail.com>
2024-08-27 14:34:49 +0900

    dev-Calendar Squashed commit
    commit 2285406f6198a02f8a3f61e06678c444909c57d9
    Author: Kwak <mjkwahk@gmail.com>
    Date:   Tue Aug 27 14:05:32 2024 +0900
    
        Calendar Sunday 색상 변경 최종
    
    commit a1b9f12b007c0995466fec0da5088b8b66932108
    Author: Kwak <mjkwahk@gmail.com>
    Date:   Tue Aug 27 13:53:27 2024 +0900
    
        Calendar Sunday 색상 변경
    
    commit c032367b801808a36fbf0e7e495e23f42c4f5ec5
    Author: Kwak <mjkwahk@gmail.com>
    Date:   Tue Aug 27 11:28:29 2024 +0900
    
        Calendar

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,9 +1,40 @@
-const Calendar = () => {
-  return (
-    <>
-      <h1>Calendar Page</h1>
-    </>
-  );
-};
+import React, { useState } from 'react';
+import ReactCalendar from 'react-calendar';
+import moment from 'moment';
+import 'react-calendar/dist/Calendar.css';
+import './Calendar.css';
 
-export default Calendar;
+export default function Calendar() {
+  const curDate = new Date();
+  const [value, onChange] = useState(curDate); 
+
+
+  const tileClassName = ({ date }) => {
+
+    if (date.getDay() === 0) {
+      return 'sunday'; 
+    }
+    if (date.getDay() === 6) {
+      return 'saturday'; 
+    }
+    return null; 
+  };
+
+  return (
+    <div className="calendar-container">
+      <ReactCalendar
+        locale="en"
+        onChange={onChange}
+        value={value}
+        next2Label={null}
+        prev2Label={null}
+        formatDay={(locale, date) => moment(date).format('D')}
+        tileClassName={tileClassName} 
+        showNeighboringMonth={false}
+      />
+      <div>
+        <p>Selected Date: {moment(value).format('YYYY-MM-DD')}</p>
+      </div>
+    </div>
+  );
+}
