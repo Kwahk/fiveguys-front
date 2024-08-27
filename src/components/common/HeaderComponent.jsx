7802b4adcac76a061e77182be5a41310514e0f71
OziinG <dlajiin@gmail.com>
2024-08-27 11:28:55 +0900

    dropdown-menu create

--- src/components/common/HeaderComponent.jsx
+++ src/components/common/HeaderComponent.jsx
@@ -2,8 +2,14 @@ import "./css/HeaderComponent.css";
 import MainLogo from "../../assets/Mainlogo.png";
 import Dropdown from "../../assets/Dropdown.png";
 import { Link } from "react-router-dom";
-
+import { useState } from "react";
 const HeaderComponent = () => {
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  const DropdownHandler = () => {
+    setDropdownVisible(!isDropdownVisible);
+  };
+
   return (
     <header className="header">
       <div className="logo-container">
@@ -23,7 +29,13 @@ const HeaderComponent = () => {
             <li className="header-item">Statistics</li>
           </Link>
           <li className="header-item">
-            <img src={Dropdown} alt="Dropdown" className="dropdown" />
+            <img src={Dropdown} alt="Dropdown" className="dropdown" onClick={DropdownHandler} />
+            <div className={`dropdown-menu ${isDropdownVisible ? "show" : ""}`}>
+              <Link to="/profile">Logout</Link>
+              {/* 구분선 추가 */}
+              <div className="dropdown-divider"></div>
+              <Link to="/settings">MyPage</Link>
+            </div>
           </li>
         </ul>
       </nav>
