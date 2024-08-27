a1b9f12b007c0995466fec0da5088b8b66932108
Kwak <mjkwahk@gmail.com>
2024-08-27 13:53:27 +0900

    Calendar Sunday 색상 변경

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -2,26 +2,19 @@ import React, { useState } from 'react';
 import ReactCalendar from 'react-calendar';
 import moment from 'moment';
 import 'react-calendar/dist/Calendar.css';
-import './Calendar.css'; // Ensure this CSS file is correctly set up for styling
+import './Calendar.css';
 
 export default function Calendar() {
-  const curDate = new Date(); // Current date
-  const [value, onChange] = useState(curDate); // Selected date
-  const [activeMonth, setActiveMonth] = useState(moment(curDate).format('YYYY-MM')); // Active month state
+  const curDate = new Date();
+  const [value, onChange] = useState(curDate); 
 
-  // Function to add custom content to each tile
-  const addContent = ({ date, view }) => {
-    if (view === 'month') {
-      // Add content to tiles if needed
-      return
+
+  const tileClassName = ({ date }) => {
+
+    if (date.getDay() === 0) {
+      return 'sunday'; 
     }
-    return null;
-  };
-
-  // Function to handle month change
-  const getActiveMonth = (activeStartDate) => {
-    const month = moment(activeStartDate).format('YYYY-MM');
-    setActiveMonth(month); // Update active month state
+    return null; 
   };
 
   return (
@@ -32,16 +25,12 @@ export default function Calendar() {
         value={value}
         next2Label={null}
         prev2Label={null}
-        formatDay={(locale, date) => moment(date).format('D')} // Format day
-        tileContent={addContent}
+        formatDay={(locale, date) => moment(date).format('D')}
+        tileClassName={tileClassName} 
         showNeighboringMonth={false}
-        onActiveStartDateChange={({ activeStartDate }) =>
-          getActiveMonth(activeStartDate) // Update active month when calendar view changes
-        }
       />
       <div>
         <p>Selected Date: {moment(value).format('YYYY-MM-DD')}</p>
-
       </div>
     </div>
   );
