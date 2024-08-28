dd0a6a0c3d40d919b0b8cbfd4c1231c7a6a5b251
김욱진 <dnrwls9115@naver.com>
2024-08-28 16:19:23 +0900

    readme

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
   plugins: [react()],
-
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    },
+    
   },
   resolve: {
     alias: {
